87901bafe8df8854b78b66754b27d18a41013d3f
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2024-02-18 22:26:41 +0100

    yet another more user-friendly countdown. Closes #12

--- display_utils/countdown_test.py
+++ display_utils/countdown_test.py
@@ -5,20 +5,20 @@ from display_utils.countdown import lit_countdown_position, countdown_position, 
     highest_position_for_seconds
 
 
-def test_19_should_lit_upper_left_segment_of_l2c3_digit():
+def test_18_should_lit_bottom_segment_of_l3c5_digit():
     display_matrix = [[0 for j in range(7)] for i in range(4)]
-    lit_countdown_position(19, display_matrix)
-    assert display_matrix[1][2] & 0b0100000 == 0b0100000
+    lit_countdown_position(18, display_matrix)
+    assert display_matrix[2][4] & 0b0001000 == 0b0001000
 
 
-def test_19_should_hide_upper_left_segment_of_l2c3_digit():
+def test_18_should_hide_bottom_segment_of_l3c5_digit():
     display_matrix = [[0b1111111 for j in range(7)] for i in range(4)]
-    hide_countdown_position(19, display_matrix)
-    assert display_matrix[1][2] | 0b1011111 == 0b1011111
+    hide_countdown_position(18, display_matrix)
+    assert display_matrix[2][4] | 0b1110111 == 0b1110111
 
 
-def test_19_position_should_be_6th_digit_on_l2c3():
-    assert countdown_position(19) == (1, 2, 5)
+def test_18_position_should_be_4th_digit_on_l3c5():
+    assert countdown_position(18) == (2, 4, 3)
 
 
 def test_print_seconds_over60():
@@ -36,10 +36,10 @@ def test_highest_positions(seconds, highest_position):
     assert highest_position_for_seconds(seconds) == highest_position
 
 
-def test_lit_all_positions_for_19pos():
+def test_lit_all_positions_for_18pos():
     display_matrix = [[0 for j in range(7)] for i in range(4)]
-    display_matrix = display_utils.countdown.lit_all_positions_for_seconds(95, display_matrix)
+    display_matrix = display_utils.countdown.lit_all_positions_for_seconds(85, display_matrix)
     assert display_matrix == [[0, 0, 0, 0, 0, 0, 0],
-                              [0, 0, 104, 28, 74, 0, 0],
-                              [0, 0, 88, 106, 76, 0, 0],
-                              [0, 0, 0, 0, 0, 0, 0]]
+                              [0, 0, 0, 0, 0, 0, 0],
+                              [0, 0, 8, 8, 8, 0, 0],
+                              [0, 73, 73, 73, 73, 73, 0]]
